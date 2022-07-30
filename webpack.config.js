0a21417a1ef389d37ec11b9c8a365dd9a37ac45f
Dmitrii Abramov <>
2022-07-30 15:27:09 +0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const baseConfig = {
+    devtool: 'eval-source-map',
+    entry: path.resolve(__dirname, './src/index.js'),
+    
+    mode: 'development',
+    module: {
+        rules: [
+            {
+				test: /\.html$/i,
+				loader: "html-loader",
+			},
+            {
+                test: /\.s[sc]ss$/,
+                use: [
+                    {
+						loader: MiniCssExtractPlugin.loader,
+					},
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js'],
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, './dist'),
+        assetModuleFilename: 'assets/[name][ext]',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html'),
+            filename: 'index.html',
+            inject: true,
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'styles/[name].css',
+        }),
+        new CleanWebpackPlugin(),
+    ],
+};
+
+module.exports = ({ mode }) => {
+    const isProductionMode = mode === 'prod';
+    const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+
+    return merge(baseConfig, envConfig);
+};
---
+++ webpack.dev.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        watchFiles: ["./src/*"],
+        port: 4000,
+        hot: true,
+    },
+};
